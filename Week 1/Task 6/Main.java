f7b0a40e2442e2ac52a271e702b7237ab87b2979
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2016-03-03 22:53:42 +0800

    Task 6

--- Week 1/Task 6/Main.java
+++ Week 1/Task 6/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        String userInput = "";
+        Scanner keyBoard = new Scanner(System.in);
+        userInput = keyBoard.nextLine();
+        System.out.println(userInput.toUpperCase());
+        System.out.println(userInput.toLowerCase());
     }
 }
